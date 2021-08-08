eebe56f4642b65d1bc0c35cd04b6a85c2ceba060
iq0x <88105363+iq0x@users.noreply.github.com>
2021-08-08 23:46:36 +0200

    Update spaceshit.js

--- spaceshit.js
+++ spaceshit.js
@@ -3,15 +3,19 @@ var ufo_clicked = false;
 var bong_clicked = false;
 var rocket_clicked = false; 
 var friend_clicked = false; 
-var level2 = 10;
+var level2 = 20;
+var alien_slaps = 0;
 
 var audio_mega_explosion = new Audio('mega_explosion.mp3');
 var audio_victory = new Audio('victory.mp3');
+var intro = new Audio('x.mp3');
+var final = new Audio('final.mp3');
 
 
 window.onload = function()
 {
     document.getElementById("score_value").innerHTML = total_hits;
+    intro.play();
 };
 
 function getRandomInt(max) {
@@ -23,6 +27,7 @@ function win()
 {
     if (total_hits == 4)
     {
+        final.play();
         document.body.style.backgroundImage="url(img/win.gif)";
         document.getElementById('bubble').style.backgroundColor="yellow";
         document.getElementById("avatar").src='img/iq9x_avatar.gif';
@@ -126,13 +131,14 @@ function explode_friend()
 
 function get_info() 
 {
-    document.getElementById("bubble_box").innerHTML = "<b>info</b><br>best resolution on 3840px * 1080px<br>mobil less 1024px <span id='red'>no effects</span> available! <br> <b>dl:</b> <br><a href='https://github.com/iq0x/cybershit'>https://github.com/iq0x/cybershit</a><br><b>donate:</b><br><a href='https://www.blockchain.com/btc/address/bc1q427d9xy8t00dnnum2e0udaxhyyatx2tfr2dmkt'>bc1q427d9xy8t00dnnum2e0udaxhyyatx2tfr2dmkt</a>";
+    document.getElementById("bubble_box").innerHTML = "<b>info</b><br>best resolution on 3840px * 1080px<br>mobil less 1024px <span id='red'>no effects</span> available! <br> <b>dl:</b> <br><a href='https://github.com/iq0x/cybershit'>https://github.com/iq0x/cybershit</a><br><b>donate:</b><br><a href='https://www.blockchain.com/btc/address/bc1q427d9xy8t00dnnum2e0udaxhyyatx2tfr2dmkt'>bc1q427d9xy8t00dnnum2e0udaxhyyatx2tfr2dmkt</a><br>";
     document.getElementById("avatar").src='img/iq9x_avatar.gif';
     
 } 
 
 function reset() 
 {
+    intro.play();
     total_hits = 0;
     ufo_clicked = false;
     bong_clicked = false;
@@ -147,7 +153,7 @@ function reset()
     document.getElementById('ufo').style.backgroundImage="url(img/ufo.gif)";
     document.getElementById('friend').style.backgroundImage="url(img/corona.png)";
     
-    document.getElementById('bubble_box').innerHTML = "Hello my name <b><i>iq0x</i></b> your personal assistent. Welcome to the cybershit universe.";
+    document.getElementById('bubble_box').innerHTML = "Hello my name <b><i>iq0x</i></b>, your personal assistent.<br>  Welcome to the cybershit universe,<br> a <b>SBFRPG</b> (stupid brain fckd role play game)";
     document.getElementById('avatar').src='img/iq9x_avatar.gif';
     document.getElementById('rocket').style.bottom="0%";
     
@@ -169,9 +175,16 @@ function punch_alien()
 {
     var audio_punch = new Audio('punch.mp3');
     audio_punch.play();
-
-    document.getElementById("bubble_box").innerHTML = "<b>autsch</b>";
-    document.getElementById("avatar").src="img/alien_avatar.gif";
+    alien_slaps++;
+    if (alien_slaps < 7)
+    {
+        document.getElementById("bubble_box").innerHTML = "<b>autsch</b>";
+        document.getElementById("avatar").src="img/alien_avatar.gif";  
+    }
+    else
+    {
+        document.getElementById("bubble_box").innerHTML = "<b>enough dude find and kill the objects....!</b>";  
+    }
 }
 
 function punch_boss1() 
@@ -204,7 +217,7 @@ function punch_boss1()
     else if (total_hits = level2)
     {
         document.getElementById('boss1').style.backgroundImage="url(img/boss_kill.gif)";
-        document.getElementById("bubble_box").innerHTML = "<b>CONGRATULATION!!!</b><br> you are the best man!!!<br>level 2 upcoming soon,";
+        document.getElementById("bubble_box").innerHTML = "<b>CONGRATULATION!!!</b><br> you are the best man!!!<br>level 2 upcoming soon, stay tuned<br><b>donate NOW and feel better:</b><br><img src='img/donate_btc.gif' width='50%'><a href='https://www.blockchain.com/btc/address/bc1q427d9xy8t00dnnum2e0udaxhyyatx2tfr2dmkt'>bc1q427d9xy8t00dnnum2e0udaxhyyatx2tfr2dmkt</a>";
         document.getElementById("avatar").src="img/iq9x_avatar.gif";
         audio_mega_explosion.play();
         audio_victory.play();
