9e622e4ed4082fe835d7b886ac1d6604f743f50b
iq0x <88105363+iq0x@users.noreply.github.com>
2021-08-13 00:45:10 +0200

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
         <meta name="viewport" content="width=devicve-width, initial-scale=1.0"> 
         <link rel="stylesheet" href="engine.css">
         <script src="terminal.js"></script>
+        
         <?php if ($_GET["level"] == "space") { ?>
                 <link rel="stylesheet" href="space.css">
                 <script src="spaceshit.js"></script>
@@ -53,17 +54,16 @@
             	   </div>
                 <?php
                 }
-            ?>
-            
-            <?php
+     
                 if ($_GET["level"] == "jungle")
                 {
                 ?> 
                     <div id="clouds"></div>
                     <div id="score">Score: <span id="score_value">0</span></div>
-                    <div id="life">life: <span id="life_value">0</span></div>
-                    <div id="ammo_maschinegun">ammo: <span id="ammo_maschinegun_value">0</span></div>
-                    <div id="ammo_pirategun">ammo: <span id="ammo_pirategun_value">0</span></div>
+                    <div id="life"><span id="life_value">0</span></div>
+                    <div id="ammo_maschinegun" onclick="alien_maschinegun()"><span id="ammo_maschinegun_value">0</span></div>
+                    <div id="ammo_pirategun" onclick="alien_pirategun()"><span id="ammo_pirategun_value">0</span></div>
+                    <div id="healing" onclick="heal()"><span id="healing_value">0</span></div>
                     <div id="monsterlife">monster: <span id="monsterlife_value">0</span></div>
                     <div id="alien"></div>
                     <div id="alien_s"></div>
@@ -71,9 +71,7 @@
                     <div id="bat" onclick="explode_bat()"></div>
                 <?php
                 }
-            ?>
-            
-            <?php
+           
             if ($_GET["level"] != "jungle" && $_GET["level"] != "space")
             {
                 ?>
@@ -88,7 +86,22 @@
                 <?php
             }
             ?>
-             <div id="bubble">
+      
+            <?php
+            if ($_GET["level"] != "jungle" && $_GET["level"] != "space")
+            {
+                ?>
+                    <script>
+               window.onload = function()
+                {
+                    document.getElementById('bubble').style.display="none";
+                }
+            </script>
+                <?php
+            }
+            ?>
+              
+            <div id="bubble">
                 <table>
                     <tr>
                         <td><img id="avatar" src="img/iq9x_avatar.gif"></td>
@@ -98,13 +111,11 @@
             </div>
            <div id="terminal">guest@cybershit:~$<input type="text" id="cmd" name="cmd"></div>
             <div id="menu">
-                <div id="menu_box" onclick="alien_maschinegun()"><img src="img/maschinegun.gif" width="80%"></div>
-                <div id="menu_box" onclick="alien_pirategun()"><img src="img/pirategun.gif" width="80%"></div>
                 <div id="menu_box" onclick="reset()"><img src="img/reset.png" width="80%"></div>
                 <div id="menu_box" onclick="toggle_terminal()"><img src="img/terminal.gif" width="80%"></div>
-                <div id="menu_box" onclick="get_info()"><img src="img/info.png" width="80%"></div>
             </div>
             <div id="question" onclick="open_menu()"></div>
+            <div id="game_over"><a href="https://cybershit.io/?level=jungle">Game Over</a></div>
         </div>
     </body>
 </html>
