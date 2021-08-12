4cc92fd11eb3ee395961e01f306342bcb02d192e
iq0x <88105363+iq0x@users.noreply.github.com>
2021-08-13 00:44:45 +0200

    Update jungleshit.js

--- jungleshit.js
+++ jungleshit.js
@@ -1,14 +1,14 @@
 var toggleMenu = false;
 var total_hits = 0;
-var ammo_maschinegun = 50;
-var ammo_pirategun = 50;
-var life = 99;
+var ammo_maschinegun = 40;
+var ammo_pirategun = 5;
+var life = 50;
+var healing = 1;
 var bat_clicked = false; 
 var monster_clicked = false;
-var monster_life = 999;
+var monster_life = 600;
 let weapon;
 var damage = 0;
-
 
 let welcome = '<b>Level 2 - <i>Welcome</i> </b>to  - jungle (beta)<br>new weapons unlocked!!!<br> show, in the menu';
 
@@ -24,9 +24,24 @@ window.onload = function()
     document.getElementById("ammo_pirategun_value").innerHTML = ammo_pirategun;
     document.getElementById("life_value").innerHTML = life;
     document.getElementById("monsterlife_value").innerHTML = monster_life;
+    document.getElementById("healing_value").innerHTML = healing;
     weapon = 'pirategun';
 };
 
+function heal()
+{
+    if (healing > 0)
+    {
+        healing--;
+        life = life + 20;
+    }
+    if (healing === 0)
+    {
+        document.getElementById('healing').style.backgroundColor="red";
+    }
+    document.getElementById("healing_value").innerHTML = healing;
+    document.getElementById("life_value").innerHTML = life;
+}
 
 function alien_maschinegun()
 {
@@ -45,6 +60,7 @@ function explode_bat()
     if (bat_clicked !== true)
     {
         check();
+        
         document.getElementById("score_value").innerHTML = total_hits;
         bat_clicked = false; 
     }
@@ -56,40 +72,60 @@ function explode_bat()
 function explode_monster() 
 {
     check();
-    monster_life = monster_life - damage;
+    if (monster_life <= 0)
+    {
+        alert("win");
+    }
     document.getElementById("monsterlife_value").innerHTML = monster_life;
     document.getElementById("score_value").innerHTML = total_hits;
-    document.getElementById("bubble_box").innerHTML = "die monster....";
+    document.getElementById("bubble_box").innerHTML = "damage: " + damage + " <br>die monster....";
     document.getElementById("avatar").src='img/alien_avatar.gif';
 }
-
 
 function check()
 {
     if (weapon == 'pirategun')
     {
-        damage = getRandomInt(10);
-        var audio_pirategun = new Audio('sounds/pirategun.mp3');
-        audio_pirategun.play();
-        ammo_pirategun--;
-        document.getElementById("ammo_pirategun_value").innerHTML = ammo_pirategun;
+        if (ammo_pirategun > 0)
+        {
+            damage = getRandomInt(100);
+            var audio_pirategun = new Audio('sounds/pirategun.mp3');
+            audio_pirategun.play();
+            ammo_pirategun--;
+            document.getElementById("ammo_pirategun_value").innerHTML = ammo_pirategun;
+            monster_life = monster_life - damage;
+            total_hits++;
+        }
+        else
+        {
+            document.getElementById('ammo_pirategun').style.backgroundColor="red";
+        }
     }
     else if (weapon == 'maschinegun')
     {
-        var audio_maschinegun = new Audio('sounds/maschinegun.mp3');
-        audio_maschinegun.play();
-        ammo_maschinegun--;
-        damage = getRandomInt(20);
-        document.getElementById("ammo_maschinegun_value").innerHTML = ammo_maschinegun;
+        if (ammo_maschinegun > 0)
+        {
+            var audio_maschinegun = new Audio('sounds/maschinegun.mp3');
+            audio_maschinegun.play();
+            ammo_maschinegun--;
+            damage = getRandomInt(20);
+            document.getElementById("ammo_maschinegun_value").innerHTML = ammo_maschinegun;
+            monster_life = monster_life - damage;
+            total_hits++;
+        }
+           else
+        {
+            document.getElementById('ammo_maschinegun').style.backgroundColor="red";
+        }
     }
-    total_hits++;
+    
     if (total_hits % 10 === 0)
     {
         var damage_alien = getRandomInt(40);
         life = life - damage_alien;
         if(life <= 0)
         {
-           alert('you lost!'); 
+           document.getElementById('game_over').style.display="block"; 
         }
         document.getElementById("life_value").innerHTML = life;
     }
@@ -98,8 +134,27 @@ function check()
 
 function reset()
 {
+    ammo_maschinegun = 40;
+    ammo_pirategun = 5;
+    life = 50;
+    healing = 1;
+    bat_clicked = false; 
+    monster_clicked = false;
+    monster_life = 600;
+    damage = 0;
     total_hits = 0;
+    weapon = 'pirategun';
+    document.getElementById('bubble_box').innerHTML = welcome;
+    document.getElementById("score_value").innerHTML = total_hits;
+    document.getElementById("ammo_maschinegun_value").innerHTML = ammo_maschinegun;
+    document.getElementById("ammo_pirategun_value").innerHTML = ammo_pirategun;
+    document.getElementById("life_value").innerHTML = life;
+    document.getElementById("monsterlife_value").innerHTML = monster_life;
+    document.getElementById("healing_value").innerHTML = healing;
     document.getElementById('bubble_box').innerHTML = welcome;
     document.getElementById("score_value").innerHTML = total_hits;
     document.getElementById('bat').style.backgroundImage="url(img/bat.gif)"; 
+    document.getElementById('ammo_pirategun').style.backgroundColor="white";
+    document.getElementById('ammo_maschinegun').style.backgroundColor="white";
+    document.getElementById('healing').style.backgroundColor="white";
 }
